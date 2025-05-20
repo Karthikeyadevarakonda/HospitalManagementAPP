5fd0158e5d6fe6cf226a4d9a924412e7b68ae120
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-05-20 10:05:39 +0530

    deploy

--- src/Hospitals.jsx
+++ src/Hospitals.jsx
@@ -3,17 +3,18 @@
 const Hospitals = ({hospitals}) => {
  console.log("WE GOT :",hospitals.hospitals)
 
+
   return (
-    <div className="w-[100%] m-auto">
+    <div className="w-[100%] m-auto py-5 bg-gray-100">
    
-    {hospitals?.city === undefined ? " " :<h1 className="text-2xl text-slate-700 text-center font-bold  my-5"><em>{"Hospitals in "+hospitals?.city}</em> </h1>}
-     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-0 gap-y-5 md:gap-y-3 place-items-center mt-5 px-5 mb-10">
+    {hospitals?.city === undefined ? " " :<h1 className="text-2xl text-slate-700 text-center font-bold  "><em>{"Hospitals in "+hospitals?.city}</em> </h1>}
+     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-0 gap-y-5 md:gap-y-8 place-items-center mt-5 px-5 mb-10">
        
 
       {hospitals?.hospitals?.map((obj)=>{
         
         return(
-           <div key={obj.hospitalId} className="rounded-md w-[270px] h-[300px]  md:w-[260px] md:h-[310px] transition-transform duration-200 hover:scale-95 overflow-hidden">
+           <div style={{background: '#f2f3f7',boxShadow: '0.6em 0.6em 1.2em #d2dce9, -0.5em -0.5em 1em #ffffff',borderRadius: '20px', }} key={obj.hospitalId} className="shadow-lg rounded-2xl transform hover:-translate-y-2 transition duration-300  w-[270px] h-[300px]  md:w-[280px] md:h-[310px]  hover:scale-95 overflow-hidden">
            <div className="w-full h-[150px] md:h-[181px] relative m-auto rounded-2xl overflow-hidden">
            <img src={obj.hospital_image} alt={obj.hospitalName} className="w-full h-full object-cover"/>
            <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent"></div>
