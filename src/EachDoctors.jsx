6da0ea46e7cbe6f95dbf217b73c19c2f92940953
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-05-21 14:37:07 +0530

    deploy

--- src/EachDoctors.jsx
+++ src/EachDoctors.jsx
@@ -9,18 +9,18 @@ const EachDoctors = () => {
 
   return (
     <div className="w-full">
-    <div className="w-[90%] shadow pb-4 pl-4 pt-4 mt-5 mb-5 mx-3.5 " style={{background: '#f2f3f7',boxShadow: '0.6em 0.6em 1.2em #d2dce9, -0.5em -0.5em 1em #ffffff',borderRadius: '20px', }} >
+    <div className="w-[90%] shadow pb-4 pl-4 pt-2 mt-5 mb-5 mx-3.5 " style={{background: '#f2f3f7',boxShadow: '0.6em 0.6em 1.2em #d2dce9, -0.5em -0.5em 1em #ffffff',borderRadius: '20px', }} >
 
     <div key={eachDoctor.doctorId} className="w-full  m-auto mt-5 flex justify-around" >
-     <img src={eachDoctor.doctorImage} alt="" className="w-[120px]  overflow-hidden rounded-full shadow"   />
+     <img src={eachDoctor.doctorImage} alt="" className="w-[100px]  overflow-hidden rounded-full shadow"   />
      
      <div className="flex flex-col justify-center pl-4 flex-1/2 ">
-       <span className="flex items-center space-x-1 text-black text-lg font-medium">
+       <span className="flex items-center space-x-1 text-black text-sm font-medium">
         <FaStar size={22} className="text-yellow-500" />
         <em><span>{eachDoctor.rating+" Rated"}</span></em>
       </span>
 
-      <p className="flex items-center space-x-2 text-lg md:text-md pl-4">
+      <p className="flex items-center space-x-2 text-sm md:text-md pl-4">
       <span><em> {eachDoctor.isAvailable ? 'Available' : 'Leave'}</em></span> <span className={`h-2 w-2 rounded-full inline-block  ${eachDoctor.isAvailable ? 'bg-green-500' : 'bg-red-500' }`}></span>
       </p>
      </div>
@@ -35,7 +35,7 @@ const EachDoctors = () => {
      
        
 
-       <div className="mt-3">
+       <div className="mt-3 pr-2">
         <p className="text-sm text-gray-600">
           <em>{ 
             eachDoctor.rating >= 4.5
